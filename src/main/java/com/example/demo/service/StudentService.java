6fa7c3afa084d03087df842680740184243a2dad
bala-amypo <bala@amypo.in>
2025-12-17 06:26:58 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -2,19 +2,8 @@ package com.example.demo.service;
 
 import java.util.List;
 import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepository;
 
-@Service
-public class StudentService {
-
-    @Autowired
-    StudentRepository repo;
-
-    public Student createData(Student stu) {
-        return repo.save(stu);
-    }
-
-    public List<Student> fetchRecord() {
-        return repo.findAll();
-    }
+public interface StudentService {
+    Student createData(Student stu);
+    List<Student> fetchRecord();
 }
